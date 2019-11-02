f4f47b38abec33f3a22cd8c16844b057eb62bd6b
Angelique <acharles-davis00@stuy.edu>
2019-11-02 14:39:31 -0400

    adding library

---
+++ SongLibraryFunctions.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "SongNodeFunctions.c"
+
+int main(){
+  return 0;
+}
---
+++ SongLibraryFunctions.h
@@ -0,0 +1,24 @@
+struct song_node {
+char name[100];
+char artist[100];
+struct song_node *next; };
+struct song_node * table[27];
+void add_song(char * artist, char *song);
+struct song_node * search_song(char *artist, char *song);
+struct song_node * search_artist(char *artist);
+void print_letter(char a);
+void print_artist(char *artist);
+void print_library();
+void shuffle();
+void delete_song();
+void clear();
+/*Add song nodes.
+Search for a song given song and artist name (return a pointer).
+Search for an artist.
+Print out all the entries under a certain letter.
+Print out all the songs of a certain artist
+Print out the entire library.
+Shuffle - print out a series of randomly chosen songs.
+Delete a song
+Clear the library.
+*/

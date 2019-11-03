683343d1019bcab74397858c5627b8bd03882f6e
Lauren <laurenpehlivanian@gmail.com>
2019-11-03 23:01:43 +0000

    debugged and fixed remove_song, got rid of free()

--- SongNodeFunctions.c
+++ SongNodeFunctions.c
@@ -35,6 +35,32 @@ void print_list(struct song_node * myNode) {
   //printf("]");
   return;
 }
+
+void debug_print_list(struct song_node * myNode) {
+  //make new node copy, so as not to modify original pointer.
+  struct song_node * newNode = myNode;
+  //if current node is null, print nothing!
+  if (newNode == NULL) {
+    printf("{}");
+    return;
+  }
+  //if it's not null:
+  //loop which stops once there is no next node.
+  while (newNode->next != NULL) {
+    printf(" %s: %s |\n",newNode->artist,newNode->name);
+    printf("NEXT:: %s: %s\n\n", newNode->next->artist, newNode->next->name);
+    newNode = newNode->next;
+  }
+  printf(" %s: %s",newNode->artist, newNode->name);
+  //there is no next node, but still need to print current (last) node:
+  //printf(" %s: %s ",newNode->artist,newNode->name);
+  printf("\n");
+  //nvm if there is no next
+  //printf("]");
+  return;
+}
+
+
 struct song_node * insert_front(struct song_node *listy,char artisty[],char songy[] ){
   struct song_node *new = newSong(artisty,songy);
 new->next=listy;
@@ -119,6 +145,55 @@ struct song_node * randomly(struct song_node *listy){
   return newNode;
 }
 
+struct song_node * remove_song(struct song_node * listy, char * artist, char * song) {
+  if (listy == NULL) return listy;
+  struct song_node * current = listy;
+  //if first node is a match:
+  if (strcmp(current->artist,artist)==0 && strcmp(current->name,song)==0) {
+    //return 2nd node (to be new start)
+    current = current->next;
+    free(listy);
+    return current;
+  }
+  //will stop at 2nd to last node(which checks last node)
+  while (current->next->next != NULL) {
+    //if artist and song matches (compare strings)
+    if (strcmp(current->next->artist,artist)==0 && strcmp(current->next->name,song)==0) {
+      //set prev node to next one after skip
+      // print_song(current); printf("   ->    "); print_song(current->next->next);
+      // printf("\nremoving: ");
+      // print_song(current->next);
+      struct song_node * temp = current;
+      current->next=current->next->next;
+      // printf("\n\n");
+      // // debug_print_list(listy);
+      // free(temp->next->artist);
+      // free(temp->next->name);
+
+      //^^ every time  Itried freeing the data, the next node's name was messed up.
+
+      // debug_print_list(listy);
+
+      // printf("\n\n");
+      // debug_print_list(listy);
+      // printf("\n%s\n",current->next->name);
+      return listy;
+    }
+    current=current->next;
+  }
+  //check the last song:
+  //was on 2nd to last node (even though already checked to see if match):
+  if (strcmp(current->next->artist,artist)==0 && strcmp(current->next->name,song)==0) {
+    //set new last node to have null next (current is new last bc it is second to last)
+    current->next = NULL;
+    free(current->next);
+    return listy;
+  }
+  //song not found:
+  return listy;
+}
+
+
 int main(){//temporary main to test as we go
 struct song_node * listy;
 struct song_node * other;
@@ -129,6 +204,20 @@ listy->next=NULL;
 //listy=insert_front(listy,"Artist 2","Song 2");
 listy=insert_song(listy, "Michael Jackson", "Thriller");
 listy=insert_song(listy, "NSYNC", "Bye Bye Bye");
+listy=insert_song(listy, "Prince", "Purple Rain");
+
+//testing remove:
+printf("------------------");
+debug_print_list(listy);
+printf("\n");
+remove_song(listy, "Michael Jackson", "Thriller");
+print_list(listy);
+printf("\n");
+remove_song(listy, "Prince", "Purple Rain");
+print_list(listy);
+
+printf("------------------------\n\n");
+
 char a = 'a';
 char b= 'b';
 char * lettera = &a;
